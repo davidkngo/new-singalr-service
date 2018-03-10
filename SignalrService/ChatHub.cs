4b5f68046e21402b31c008817976686a73829610
David Ngo <davidkngo@outlook.com>
2018-03-10 13:56:12 +0700

    Change in Chathub

--- SignalrService/ChatHub.cs
+++ SignalrService/ChatHub.cs
@@ -5,9 +5,33 @@ namespace SignalrService
 {
     public class ChatHub : Hub
     {
+
+        public int OnCounter = 0;
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.InvokeAsync("ShowUsersOnline", ++OnCounter);
+            await base.OnConnectedAsync();
+        }
+
         public async Task SendToAll(ChatMessage message)
         {
-            await Clients.All.InvokeAsync("Send", message);
+            await Clients.All.InvokeAsync("SendToAll", message);
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendToGroup(string groupName, ChatMessage message)
+        {
+            await Clients.Group(groupName).InvokeAsync("SendToGroup", message);
         }
     }
 }
