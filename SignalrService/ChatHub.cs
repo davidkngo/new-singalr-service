29d6fd0b7062403c6d79f5823eb9b86c16b8ac1a
David Ngo <davidkngo@outlook.com>
2018-03-10 10:23:22 +0700

    First Commission

---
+++ SignalrService/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalrService
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendToAll(ChatMessage message)
+        {
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
---
+++ SignalrService/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrService
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+    }
+}
